4617a6e9eda1e2d8604b14987a59c63dbc03e34e
Jsofeng <jonathan.jf.zhao@gmail.com>
2025-08-21 13:08:40 -0400

    Layout

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+  public static void main(String[] args) {
+
+    System.out.println("*****************");
+    System.out.println("AMERICAN EXPRESS");
+    System.out.println("1: CHECK BALANCE");
+    System.out.println("2: DEPOSIT");
+    System.out.println("3: WITHDRAW");
+    System.out.println("4: EXIT"):
+    System.out.println("******************");
+    System.out.println("CHOOSE A NUMBER 1 - 4");
+
+  }
+}
