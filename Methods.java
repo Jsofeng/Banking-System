6aaabddc8d004fc30a172cffb87b2b7f557449ce
Jsofeng <jonathan.jf.zhao@gmail.com>
2025-08-21 13:17:03 -0400

    Methods added

---
+++ Methods.java
@@ -0,0 +1,20 @@
+public class Main {
+  
+  public static void main(String[] args) {
+  }
+
+  static void showBalance(double balance) {
+    
+  }
+
+  static double deposit() {
+    
+  }
+
+  static double withDraw(double balance) {
+    
+  }
+
+  
+  
+}
