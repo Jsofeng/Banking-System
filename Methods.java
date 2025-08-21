9b690a1d5f2f2b1ee3a39f776c79b04a44036506
Jsofeng <jonathan.jf.zhao@gmail.com>
2025-08-21 13:24:30 -0400

    Methods complete

--- Methods.java
+++ Methods.java
@@ -4,17 +4,39 @@ public class Main {
   }
 
   static void showBalance(double balance) {
-    
+    System.out.println("**************");
+    System.out.println("%.2f%\n", balance);
   }
 
   static double deposit() {
-    
+    System.out.println("ENTER AMOUNT TO DEPOSIT");
+    double amount = scan.nextDouble();
+    if(amount < 0) {
+      System.out.println("Please try again");
+      return deposit();
+      
+    } else {
+      return amount;
   }
 
   static double withDraw(double balance) {
+    System.out.println("***************");
+    double amount = scan.nextDouble();
+    if(amount > balance) {
+      System.out.println("INSUFFICIENT FUNDS");
+      return withDraw(balance);
     
+    } else if(amount < 0) {
+      System.out.println("PLEASE TRY AGAIN");
+      return withDraw(balance);
+    
+    } else {
+      System.out.println("BALANCE AFTER WITHDRAW: $" + (balance - amount));
+      return amount;
+    } 
   }
 
   
   
 }
+
