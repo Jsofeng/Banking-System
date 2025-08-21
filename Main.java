af9945f142b2e13c3bd8c8375ad24035de01ef03
Jsofeng <jonathan.jf.zhao@gmail.com>
2025-08-21 13:14:41 -0400

    cases

--- Main.java
+++ Main.java
@@ -1,6 +1,10 @@
 public class Main {
   public static void main(String[] args) {
-
+    static Scanner scan = new Scanner(System.in);
+    double balance;
+    boolean isOpen = true;
+    int choice;
+    
     System.out.println("*****************");
     System.out.println("AMERICAN EXPRESS");
     System.out.println("1: CHECK BALANCE");
@@ -9,6 +13,19 @@ public class Main {
     System.out.println("4: EXIT"):
     System.out.println("******************");
     System.out.println("CHOOSE A NUMBER 1 - 4");
+    
+    choice = scan.nextInt();
 
+    while(isOpen) {
+      switch(choice) {
+        case 1 -> 
+        case 2 ->
+        case 3 ->
+        case 4 ->
+        default -> System.out.println("INVALID NUMBER CHOOSE BETWEEN 1 - 4");
+      }
+    }
+    scan.close();
   }
 }
+
