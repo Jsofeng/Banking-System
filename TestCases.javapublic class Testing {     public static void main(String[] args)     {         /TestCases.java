ecde186db708b496524d0eade9191501acd54471
Jonathan <jonathan.jf.zhao@gmail.com>
2025-08-31 17:44:36 -0400

    Create TestCases.java
    testcases

---
+++ TestCases.javapublic class Testing {     public static void main(String[] args)     {         /TestCases.java
@@ -0,0 +1,28 @@
+/*
+public class Testing
+{
+    public static void main(String[] args)
+    {
+        // Example usage
+        BankingMethods account = new BankingMethods("123456789", "John Doe");
+        double johnBalance = 0;
+        account.setAccountInfo("123456789", "John Doe");
+        double johndeposit = BankingMethods.deposit();
+        account.balance += johndeposit;
+        System.out.println(account.accountDetails());
+        account.recordTransaction("Deposited: $" + johndeposit);
+
+        BankingMethods newAccount = new BankingMethods("987654321", "Jane Smith");
+        double janeBalance = 0;
+        newAccount.setAccountInfo("987654321", "Jane Smith");
+        double janedeposit = BankingMethods.deposit();
+        newAccount.balance += janedeposit;
+        System.out.println(newAccount.accountDetails());
+        account.recordTransaction("Deposited: $" + janedeposit);
+        account.transferFunds(newAccount, janedeposit);
+        account.recordTransaction("Transferred: $" + janedeposit + " to account " + newAccount.accountNumber);
+
+
+    }
+}
+*/
