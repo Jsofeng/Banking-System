180eb7d9d80303b6662f38c5b4256fb7fc18a93e
Jonathan <jonathan.jf.zhao@gmail.com>
2025-09-14 00:37:51 -0400

    Create DataBase.java

---
+++ DataBase.java
@@ -0,0 +1,18 @@
+abstract class DataBase {
+    String accountNumber;
+    String accountHolder;
+    double balance;
+
+    DataBase(String accountNumber, String accountHolder) {
+        this.accountNumber = accountNumber;
+        this.accountHolder = accountHolder;
+        this.balance = 0;
+    }
+
+    abstract void deposit(double amount);
+    abstract void withdraw(double amount);
+
+    void showBalance() {
+        System.out.printf("[%s] BALANCE: $%.2f%n", accountHolder, balance);
+    }
+}
