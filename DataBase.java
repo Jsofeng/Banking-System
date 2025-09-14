e0dda5df3524ba291b551c7ba116b540a480cd12
Jonathan <jonathan.jf.zhao@gmail.com>
2025-09-14 00:42:37 -0400

    Update DataBase.java

--- DataBase.java
+++ DataBase.java
@@ -34,3 +34,21 @@ class ChequingAccount extends DataBase {
         System.out.printf("[%s] Chequing Withdrawal: $%.2f%n", accountHolder, amount);
     }
 }
+
+class SavingsAccount extends DataBase {
+    SavingsAccount(String accountNumber, String accountHolder) {
+        super(accountNumber, accountHolder);
+    }
+
+    @Override
+    void deposit(double amount) {
+        balance += amount;
+        System.out.printf("[%s] Deposited $%.2f into Savings Account%n", accountHolder, amount);
+    }
+
+    @Override
+    void withdraw(double amount) {
+        balance -= amount;
+        System.out.printf("[%s] Withdrew $%.2f from Savings Account%n", accountHolder, amount);
+    }
+}
