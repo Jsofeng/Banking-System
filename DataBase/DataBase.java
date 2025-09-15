0b0ad45c584916977ab0b9e215776af0ea950f6c
Jonathan <jonathan.jf.zhao@gmail.com>
2025-09-15 03:45:56 +0000

    DataBase

---
+++ DataBase/DataBase.java
@@ -0,0 +1,26 @@
+public abstract class DataBase {
+    String accountNumber;
+    String accountHolder;
+    double balance;
+
+    DataBase(String accountNumber, String accountHolder) {
+        this.accountNumber = accountNumber;
+        this.accountHolder = accountHolder;
+        this.balance = 0;
+    }
+
+    abstract void deposit(double amount);
+    abstract void withdraw(double amount);
+
+    void showBalance() {
+        System.out.printf("[%s] BALANCE: $%.2f%n", accountHolder, balance);
+    }
+
+    @Override
+    public String toString() {
+        return "AccountNumber: " + accountNumber +
+                ", AccountHolder: " + accountHolder +
+                ", Balance: $" + String.format("%.2f", balance);
+    }
+
+}
