c68f3aed7f912d4692200b93e3b76e4cbbedfa9c
Jonathan <jonathan.jf.zhao@gmail.com>
2025-09-19 03:12:05 +0000

    +etransfer methods built

--- DataBase/ChequingAccount.java
+++ DataBase/ChequingAccount.java
@@ -16,6 +16,10 @@ class ChequingAccount extends DataBase {
     }
     @Override
     void eTransfer(DataBase toAccount, double transferAmount) {
-        
+        balance -= transferAmount;
+        toAccount.balance += transferAmount;
+        System.out.printf("[%s] transferred $%.2f to [%s]%n", this.accountNumber, transferAmount, toAccount.accountNumber);
+        System.out.printf("[%s] NEW BALANCE: $%.2f%n[%s] NEW BALANCE $%.2f%n", this.accountNumber, balance, toAccount.accountNumber, toAccount.balance);
     }
 }
+
