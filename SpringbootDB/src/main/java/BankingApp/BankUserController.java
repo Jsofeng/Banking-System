cbc4efa99d1b4ce67b60a2be091ae2447a8b685a
Jonathan Zhao <jonathan.jf.zhao@gmail.com>
2025-11-12 23:09:53 -0500

    Added new BankUserController.java

--- SpringbootDB/src/main/java/BankingApp/BankUserController.java
+++ SpringbootDB/src/main/java/BankingApp/BankUserController.java
@@ -1,45 +1,60 @@
-import org.springframework.web.bind.annotation.*;
-
+package com.example.springbootdb.BankingApp;
+import java.util.ArrayList;
 import java.util.List;
 
+import FileManager.FileManager;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
-@RequestMapping("api/v1/banking-user")
-public class BankUserInfoController {
-
-    private final BankUserInfoService BankUserInfoService;
-
-    public BankUserInfoController(BankUserInfoService BankUserInfoService) {
-        this.BankUserInfoService = BankUserInfoService;
-    }
-
+@RequestMapping("api/v1/banking-users")
+public class BankingUserController {
     @GetMapping
-    public List<BankUserInfo> BankingUser() {
-        return BankUserInfoService.getAllBankUserInfos();
+    public List<BankingUser> getEngineers() {
+        return List.of(new BankingUser("446916010129332", "Jonathan", 27, 10000),
+                new BankingUser("298318321932132", "Joji", 67, 200000),
+                new BankingUser("8231939123", "Leclerc", 16, 500000),
+                new BankingUser("29873839198312", "Max Verstappen", 1, 823281));
     }
 
-    @GetMapping("{id}")
-    public BankUserInfo getBankUserInfo(
-            @PathVariable Integer id
-    ) {
-        return BankUserInfoService.getBankUserInfoById(id);
+    /*
+    @GetMapping("/display")
+    public List<String> displayUserInfo() {
+        BankingUser account1 = new BankingUser("123456789", "Lewis Hamilton", 44, 9012932);
+        BankingUser account2 = new BankingUser("16394392423", "Charles Leclerc", 16, 281313);
+        return List.of(account1.accountDetails(), account2.accountDetails());
+    }
+*/
+    @GetMapping(value = "/display", produces = "text/plain") // formats it so that after each statement it adds \n
+    public String displayUserInfo() {
+
+        ArrayList<BankingUser> accounts = new ArrayList<>();
+        accounts.add(new BankingUser("123456789", "Lewis Hamilton", 44, 9012932));
+        accounts.add(new BankingUser("1601923921", "Charles Leclerc", 16, 16161616));
+        accounts.add(new BankingUser("0101010101", "Max Verstappen", 1, 1001010101));
+
+        StringBuilder sb = new StringBuilder();
+        for (BankingUser u : accounts) {
+            sb.append(u.accountDetails()).append("\n\n");
+        }
+
+        return sb.toString();
     }
 
+    @GetMapping(value = "/save", produces = "text/plain")
+    public String fileReading() {
+        ArrayList<BankingUser> accounts = new ArrayList<>();
+        accounts.add(new BankingUser("5152006", "Kang Haerin", 15, 92013201));
+        accounts.add(new BankingUser("08262005", "Pharita", 26, 2925232));
 
-    @PostMapping
-    public void addNewBankUserInfo(
-            @RequestBody BankUserInfo BankUserInfo
-    ) {
-        BankUserInfoService.insertBankUserInfo(BankUserInfo);
+        FileManager.saveAccounts("wives.txt", accounts);
+        StringBuilder sb = new StringBuilder();
+        sb.append("ACCOUNTS SAVED: \n\n");
+        for(BankingUser u : accounts) {
+            sb.append(u.accountDetails()).append("\n\n");
+        }
+        return sb.toString();
     }
 
-    @DeleteMapping("{id}")
-    public void deleteBankUserInfo(@PathVariable Integer id) {
-        BankUserInfoService.deleteBankUserInfo(id);
-    }
-
-    @PutMapping("{id}")
-    public void updateBankUserInfo(@PathVariable Integer id,
-                                       @RequestBody BankUserInfo BankUserInfo) {
-        BankUserInfoService.updateBankUserInfo(id, BankUserInfo);
-    }
 }
