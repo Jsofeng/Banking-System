410ea1d3996cf3e3e27bbb88cad2a77897287f53
Jonathan Zhao <Jonathan.jf.zhao@gmail.com>
2025-12-15 23:39:46 -0500

    Fixed format

--- SpringbootDB/src/main/java/FileManager/FileManager.java
+++ SpringbootDB/src/main/java/FileManager/FileManager.java
@@ -148,7 +148,7 @@ public class FileManager {
     public static void eTransaction(AccountType fromAccount, AccountType toAccount, double amount) {
         try(BufferedWriter bw = new BufferedWriter(new FileWriter("e-transfer.txt", true))) {
             String currentTime = LocalDateTime.now().format(dtf);
-            bw.write("[" + fromAccount.accountHolder + "]" + " e-Transfered" + " [" + toAccount.accountHolder + "]" + String.format("%.2f", amount) + " At [" + currentTime + "]\n");
+            bw.write("[" + fromAccount.accountHolder + "]" + " e-Transfered" + " [" + toAccount.accountHolder + "] $" + String.format("%.2f", amount) + " At [" + currentTime + "]\n");
 
         } catch (IOException e) {
             System.out.println("Error writing to file." + e.getMessage());
