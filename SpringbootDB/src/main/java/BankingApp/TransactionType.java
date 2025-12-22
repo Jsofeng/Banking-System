e1395d1e04e1c57c9e67fbd78e93e0992392208c
Jonathan Zhao <Jonathan.jf.zhao@gmail.com>
2025-12-21 23:00:41 -0500

    Implementing Transaction retriever

---
+++ SpringbootDB/src/main/java/BankingApp/TransactionType.java
@@ -0,0 +1,11 @@
+package com.example.bankingsystemsb;
+
+public enum TransactionType { // enum is a a special type of class used to define a fixed collection of constants (PENDING, APPROVED, etc)
+    DEPOSIT,
+    WITHDRAW,
+    TRANSFER_IN,
+    TRANSFER_OUT
+}
+
+
+
