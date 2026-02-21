1f7e1b786e908938bf32d559534430de597342f8
Jonathan Zhao <Jonathan.jf.zhao@gmail.com>
2026-02-20 21:35:01 -0500

    Added JWT AUTH

--- SpringbootDB/src/main/java/security/JwtUtil.java
+++ SpringbootDB/src/main/java/security/JwtUtil.java
@@ -1,11 +1,13 @@
 package com.example.bankingsystemsb.security;
 
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.security.Key;
 import java.util.Date;
 
 @Component
@@ -27,6 +29,10 @@ public class JwtUtil {
                 .compact();
     }
 
+    public Key getSigningKey() {
+        return Keys.hmacShaKeyFor(SECRET.getBytes());
+    }
+
     public String extractUsername(String token) {
         return Jwts.parserBuilder()
                 .setSigningKey(SECRET.getBytes())
@@ -34,5 +40,19 @@ public class JwtUtil {
                 .parseClaimsJws(token)
                 .getBody()
                 .getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(SECRET.getBytes())
+                    .build()
+                    .parseClaimsJws(token);
+
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+
     }
 }
